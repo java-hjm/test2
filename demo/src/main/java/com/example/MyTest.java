3661667ab99e0ae003342817d68c2bf1ce4d40d4
hjm <1025649341@qq.com>
2019-10-11 15:41:12 +0800

    github测试项目第一次

---
+++ demo/src/main/java/com/example/MyTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Date;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("adsd");
+        Date d = new Date();
+        System.out.print(1);
+    }
+}
---
+++ demo/src/main/java/com/example/MyTest3.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Date;
+
+public class MyTest3 {
+    public static void main(String[] args) {
+        System.out.println("adsd");
+        Date d = new Date();
+        System.out.print(1);
+    }
+}
